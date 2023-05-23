541771723d266be531334d0092914eb1c1ad2201
Heverton Rodrigues Lopes <heverton_rl@hotmail.com>
2023-05-22 23:24:45 -0300

    Final da Sessão 2 - Aula 7

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import Nome from './components/Nome'
+import Nome from './components/Nome';
+import { useState } from 'react';
 
 function App() {
+  const [aluno, setAluno] = useState('Sujeito Programador');
+
+  function handleChangeName(nome) {
+    setAluno(nome);
+  }
+
   return (
     <div>
       <h1>Component App</h1>
-      <Nome nome='Heverton' idade='35' />
+      <h2>Olá: {aluno}</h2>
+      <button onClick={() => handleChangeName('Heverton') }>Mudar o Nome</button>
     </div>
   );
 }
